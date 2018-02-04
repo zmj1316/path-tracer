452c623be6586ca0f9760ccbd49892037654f5cd
Key Zhang <zmj1316@gmail.com>
2018-02-04 19:21:26 +0800

    rename project

---
+++ CS.cpp
@@ -0,0 +1,192 @@
+//--------------------------------------------------------------------------------------
+// File: EmptyProject11.cpp
+//
+// Empty starting point for new Direct3D 9 and/or Direct3D 11 applications
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+#include "DXUT.h"
+#include "imgui.h"
+#include "imgui_impl_dx11.h"
+
+//--------------------------------------------------------------------------------------
+// Reject any D3D11 devices that aren't acceptable by returning false
+//--------------------------------------------------------------------------------------
+bool CALLBACK IsD3D11DeviceAcceptable( const CD3D11EnumAdapterInfo *AdapterInfo, UINT Output, const CD3D11EnumDeviceInfo *DeviceInfo,
+                                       DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Called right before creating a D3D9 or D3D11 device, allowing the app to modify the device settings as needed
+//--------------------------------------------------------------------------------------
+bool CALLBACK ModifyDeviceSettings( DXUTDeviceSettings* pDeviceSettings, void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D11 resources that aren't dependant on the back buffer
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D11CreateDevice( ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc,
+                                      void* pUserContext )
+{
+	ImGui_ImplDX11_Init(DXUTGetHWNDDeviceWindowed(), pd3dDevice, DXUTGetD3D11DeviceContext());
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D11 resources that depend on the back buffer
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D11ResizedSwapChain( ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
+                                          const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext )
+{
+	ImGui_ImplDX11_InvalidateDeviceObjects();
+	ImGui_ImplDX11_CreateDeviceObjects();
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle updates to the scene.  This is called regardless of which D3D API is used
+//--------------------------------------------------------------------------------------
+void CALLBACK OnFrameMove( double fTime, float fElapsedTime, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render the scene using the D3D11 device
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D11FrameRender( ID3D11Device* pd3dDevice, ID3D11DeviceContext* pd3dImmediateContext,
+                                  double fTime, float fElapsedTime, void* pUserContext )
+{
+    // Clear render target and the depth stencil 
+    float ClearColor[4] = { 0.176f, 0.196f, 0.667f, 0.0f };
+
+    ID3D11RenderTargetView* pRTV = DXUTGetD3D11RenderTargetView();
+    ID3D11DepthStencilView* pDSV = DXUTGetD3D11DepthStencilView();
+    pd3dImmediateContext->ClearRenderTargetView( pRTV, ClearColor );
+    pd3dImmediateContext->ClearDepthStencilView( pDSV, D3D11_CLEAR_DEPTH, 1.0, 0 );
+
+	ImGui_ImplDX11_NewFrame();
+	{
+		static float f = 0.0f;
+		ImGui::Text("Hello, world!");
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	}
+	ImGui::Render();
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D11 resources created in OnD3D11ResizedSwapChain 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D11ReleasingSwapChain( void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D11 resources created in OnD3D11CreateDevice 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D11DestroyDevice( void* pUserContext )
+{
+	ImGui_ImplDX11_Shutdown();
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle messages to the application
+//--------------------------------------------------------------------------------------
+extern LRESULT ImGui_ImplDX11_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+                          bool* pbNoFurtherProcessing, void* pUserContext )
+{
+	ImGui_ImplDX11_WndProcHandler(hWnd, uMsg, wParam, lParam);
+	if (uMsg == WM_LBUTTONDOWN && ImGui::GetIO().WantCaptureMouse)
+		return 0;
+	if (uMsg == WM_KEYDOWN && ImGui::GetIO().WantCaptureKeyboard)
+		return 0;
+    return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle key presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnKeyboard( UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle mouse button presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnMouse( bool bLeftButtonDown, bool bRightButtonDown, bool bMiddleButtonDown,
+                       bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
+                       int xPos, int yPos, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Call if device was removed.  Return true to find a new device, false to quit
+//--------------------------------------------------------------------------------------
+bool CALLBACK OnDeviceRemoved( void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Initialize everything and go into a render loop
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+{
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+    // DXUT will create and use the best device (either D3D9 or D3D11) 
+    // that is available on the system depending on which D3D callbacks are set below
+
+    // Set general DXUT callbacks
+    DXUTSetCallbackFrameMove( OnFrameMove );
+    DXUTSetCallbackKeyboard( OnKeyboard );
+    DXUTSetCallbackMouse( OnMouse );
+    DXUTSetCallbackMsgProc( MsgProc );
+    DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
+    DXUTSetCallbackDeviceRemoved( OnDeviceRemoved );
+
+    // Set the D3D11 DXUT callbacks. Remove these sets if the app doesn't need to support D3D11
+    DXUTSetCallbackD3D11DeviceAcceptable( IsD3D11DeviceAcceptable );
+    DXUTSetCallbackD3D11DeviceCreated( OnD3D11CreateDevice );
+    DXUTSetCallbackD3D11SwapChainResized( OnD3D11ResizedSwapChain );
+    DXUTSetCallbackD3D11FrameRender( OnD3D11FrameRender );
+    DXUTSetCallbackD3D11SwapChainReleasing( OnD3D11ReleasingSwapChain );
+    DXUTSetCallbackD3D11DeviceDestroyed( OnD3D11DestroyDevice );
+
+    // Perform any application-level initialization here
+
+    DXUTInit( true, true, NULL ); // Parse the command line, show msgboxes on error, no extra command line params
+    DXUTSetCursorSettings( true, true ); // Show the cursor and clip it when in full screen
+    DXUTCreateWindow( L"CS11" );
+
+    // Only require 10-level hardware
+    DXUTCreateDevice(D3D_FEATURE_LEVEL_11_0, true, 640, 480 );
+    DXUTMainLoop(); // Enter into the DXUT ren  der loop
+
+    // Perform any application-level cleanup here
+
+    return DXUTGetExitCode();
+}
+
+
